0a195f5f090027d049be1c8917fdaddcae70a8a3
nguyễn thái nhân <ntnhan90@gmail.com>
2023-09-19 20:41:02 +0700

    update 1

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => (
+    <section className="error-page">
+      <div className="container">
+        <h1>Error 404</h1>
+        <p>Woops. Looks like this page doesn't exist</p>
+        <a href="#" className="btn btn--rounded btn--yellow">Go to home</a>
+      </div>
+    </section>
+)
+
+export default Dashboard
