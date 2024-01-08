92a0d5b30f87d9eeadd5964056b31aa8289d6b08
nguyễn thái nhân <ntnhan90@gmail.com>
2024-01-08 23:14:24 +0700

    refactor to next 14
    remove and customer from folders page to folder app

--- src/pages/pokemons/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, CardBody, CardHeader } from 'react-bootstrap';
-
-export default function Page(){
-	return (
-		<Card>
-			<CardHeader> Add New Pokemon</CardHeader>	
-		</Card>
-	)
-}
